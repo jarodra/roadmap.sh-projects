8b7f4c18af5e5f0417eaca67bc915c8d3dcb810f
Javier <jron@gft.com>
2025-01-09 14:48:59 +0100

    Initial commit

---
+++ DevOps Roadmap/Server Performance Stats/server-stats.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Total CPU usage
+# Total memory usage (Free vs Used including percentage)
+# Total disk usage (Free vs Used including percentage)
+# Top 5 processes by CPU usage
+# Top 5 processes by memory usage
+# Extras:
+#   os version
+#   uptime, 
+#   load average
+#   logged in users
+#   failed login attempts
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# roadmap.sh
+# Projects from roadmap.sh
+Resolution of projects from [roadmap.sh](https://roadmap.sh) Project Ideas classified by Roadmap.
